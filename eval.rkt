a949f783224c40b7af94f0e22e250db7702f1689
Quin-nL <quinnlacrampe@gmail.com>
2025-05-03 10:46:09 -0700

    Lab 4
    Added support for the conditional statements cond and if.

---
+++ eval.rkt
@@ -0,0 +1,61 @@
+#lang racket
+;;Quinn Lacampe
+;;4/29/2025
+;;Lab 4
+;;e1 environment and add
+(define add
+  (lambda (a b)
+    (cond ((number? a) (+ a b))
+          ((list? a) (append a b))
+          (else (error "unable to add" a b)))))
+(define e1  (map list
+                 '(     x  y  z ls         + - * cons car cdr nil list add = nil? else)
+                 (list 10 20 30 (list 1 2) + - * cons car cdr '() list add = empty? #t)))
+;;function to lookup symbols
+(define(lookup sym env)
+(cond 
+  [(equal? (length env) 0)(error "No envirorment provided")] 
+  [(not(symbol? sym)) (error "Not a Symbol" sym)]
+  [(equal? (caar env) sym)(cadar env) ] 
+  [else (lookup sym (cdr env))])
+)
+;;offers support for cond and its nested statements
+(define (cond-helper C-item env)
+  (cond
+    [(equal? (caar C-item) 'else) (evaluate (cadar C-item) env)]
+    [else (if (equal? (evaluate (caar C-item) env) #t)
+              (evaluate (cadar C-item) env)
+              (cond-helper (cdr C-item) env))
+          ]))
+;;evaluate conditional statements.
+(define (evaluate-special-form item env) 
+  (cond 
+    [(equal? (car item) 'if) 
+        (if (equal? (evaluate (cadr item) env) #t) 
+          (evaluate (caddr item) env)
+          (evaluate (cadddr item) env))]
+      [(equal? (car item) 'cond)
+       (if (equal? (evaluate (caadr item) env) #t)
+           (evaluate (cadadr item) env)
+           (cond-helper (cddr item) env))] 
+      [else error "special form not recognized"]
+  ))
+;function to check if a list starts with a special form
+(define (special-form? input)
+  (if (or (equal? (car input) 'if )
+          (equal? (car input) 'cond))
+        #t 
+        #f))
+(define(evaluate expr env)
+  (cond
+    [(number? expr)expr]
+    [(symbol? expr)(lookup expr env)]
+    [(special-form? expr)(evaluate-special-form expr env)]
+    [(list? expr) 
+     (if (procedure? (evaluate (car expr)env))
+         (apply(evaluate (car expr) env)(map (lambda (john)  (evaluate john env)) (cdr expr)))
+         (error "First item in a list must be a procedure"(car expr)))])
+  )
+;;provide functions for tests
+(provide evaluate lookup special-form? evaluate-special-form)
+
